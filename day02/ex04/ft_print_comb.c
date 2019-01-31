aff99e717d33a7e14409b28b263c6a78e0e3c859
Socorro Woman <swoman@e3r4p7.21-school.ru>
2019-01-31 18:44:53 +0300

    ex04 done

---
+++ day02/ex04/ft_print_comb.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swoman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 16:01:02 by swoman            #+#    #+#             */
+/*   Updated: 2019/01/31 18:30:15 by swoman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	putchar_converted(int n)
+{
+	char c;
+
+	c = n + '0';
+	ft_putchar(c);
+}
+
+int		last_num(int x, int y)
+{
+	int z;
+
+	z = 3;
+	while (z <= 9)
+	{
+		if (x < y && y < z && x != z)
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+			putchar_converted(x);
+			putchar_converted(y);
+			putchar_converted(z);
+			z++;
+		}
+		else
+		{
+			z++;
+		}
+	}
+	return (z);
+}
+
+void	ft_print_comb(void)
+{
+	int x;
+	int y;
+	int z;
+
+	x = 0;
+	y = 1;
+	ft_putchar('0');
+	ft_putchar('1');
+	ft_putchar('2');
+	while (x <= 7)
+	{
+		while (y <= 8)
+		{
+			last_num(x, y);
+			z = 0;
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(void)
+{
+	ft_print_comb();
+	return (0);
+}
