3acd41742f5dea5e083764cdd76993d97a0ef38d
Socorro Woman <swoman@e3r4p7.21-school.ru>
2019-01-31 14:54:44 +0300

    ex01 done

---
+++ day02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swoman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 14:38:10 by swoman            #+#    #+#             */
+/*   Updated: 2019/01/31 14:52:37 by swoman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+	ft_putchar('\n');
+}
