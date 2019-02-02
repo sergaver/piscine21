98da91d3c4cd6578c378312ca03fbc03ebf7a48d
Socorro Woman <swoman@e3r5p7.21-school.ru>
2019-02-02 16:37:53 +0300

    ex04 done

---
+++ day04/ex02/ft_iterative_power.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swoman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/02 15:07:55 by swoman            #+#    #+#             */
+/*   Updated: 2019/02/02 16:33:43 by swoman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_iterative_power(int nb, int power)
+{
+	int res;
+
+	res = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	if (power == 1)
+	{
+		return (nb);
+	}
+	while (power > 1)
+	{
+		res = res * nb;
+		power--;
+	}
+	return (res);
+}
