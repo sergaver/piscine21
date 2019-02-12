12ec476d5e6237e4ea9b5070698fad98cb3ef59c
Socorro Woman <swoman@e6r7p6.21-school.ru>
2019-02-12 16:49:57 +0300

    ex00 done

---
+++ day10/ex00/includes/header.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swoman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/12 14:56:53 by swoman            #+#    #+#             */
+/*   Updated: 2019/02/12 15:05:01 by swoman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+
+#endif
